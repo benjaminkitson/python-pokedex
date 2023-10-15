0193393efea8fb68b8b35ba842ffedc8f3281386
Benjamin Kitson <benjaminkitson13@gmail.com>
2023-10-15 18:49:44 +0100

    some additions

--- main.py
+++ main.py
@@ -1,17 +1,65 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import yaml
 import boto3
+import json
 
-BUCKET_NAME = "bk-pokedex-bucket"
+try:
+    with open('config.yml', 'r') as file:
+        config = yaml.safe_load(file)
+except FileNotFoundError:
+    config = {
+        "ACCESS_KEY_ID": None,
+        "ACCESS_KEY_SECRET": None
+    }
 
-client = boto3.client('s3')
 
-response = client.get_object(Bucket=BUCKET_NAME, Key='pokedex.json')
+client = boto3.client(
+    's3', aws_access_key_id=config["ACCESS_KEY_ID"], aws_secret_access_key=config["ACCESS_KEY_SECRET"])
+
+
+response = client.get_object(Bucket="bk-pokedex-bucket", Key='pokedex.json')
 
 pokedex = response['Body'].read().decode('utf-8')
 
 app = Flask(__name__)
 
 
+@app.route('/api')
+def get_all_pokemon():
+    all_pokemon = json.loads(pokedex)["pokemon"]
+
+    name = request.args.get("name")
+
+    return_pokemon = None
+    if (name):
+        for pokemon in all_pokemon:
+            if (pokemon["name"].lower() == name):
+                return_pokemon = pokemon
+
+    id = request.args.get("id")
+    if (id):
+        for pokemon in all_pokemon:
+            if (pokemon["id"].lower() == id):
+                if (return_pokemon and pokemon["name"] != return_pokemon["name"]):
+                    return {"message": "No pokemon found"}, 404
+                return_pokemon = pokemon
+
+    if (return_pokemon == None):
+        return_pokemon = all_pokemon
+
+    return jsonify(return_pokemon)
+
+
 @app.route('/')
-def get_pokedex():
-    return jsonify(pokedex)
+def catch_root():
+    return {"message": "Nothing here!"}, 404
+
+
+@app.route('/<path:path>')
+def catch_all(path):
+    return {"message": "Nothing here!"}, 404
+
+
+# The config is only used in development, so only run the dev server if it "exists"
+if (config["ACCESS_KEY_ID"] and config["ACCESS_KEY_SECRET"]):
+    app.run()
